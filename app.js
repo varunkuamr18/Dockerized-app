5926852ecac9b4f01f9ba3aa3f379d5a4d807549
varunkuamr18 <varunbojjireddy98@gmail.com>
2024-12-23 14:19:54 +0530

    updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-  res.end('Hello, GitHub Actions!');
+  res.end('Hello, CI/CD');
 });
 server.listen(3000, () => console.log('Server running on port 3000'));
 
